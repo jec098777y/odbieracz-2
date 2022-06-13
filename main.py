1436e7d83790e01378fb684005b5fd9ac3ebc52b
jec098777y <106841315+jec098777y@users.noreply.github.com>
2022-06-13 18:14:20 +0200

    Aktualizacja pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -5,36 +5,35 @@ def jedzdotylu():
             pause(1000)
             robotbit.motor_run_dual(robotbit.Motors.M1A, 0, robotbit.Motors.M2A, 0)
             odleglosc2 = sonar.ping(DigitalPin.P13, DigitalPin.P15, PingUnit.CENTIMETERS)
+            
             if odleglosc2 < 10:
                 
                 robotbit.geek_servo(robotbit.Servos.S1, -25)
 
                 odleglosc2 = sonar.ping(DigitalPin.P13, DigitalPin.P15, PingUnit.CENTIMETERS)
+                pause(500)
+                robotbit.geek_servo(robotbit.Servos.S1, 25)
+                odleglosc1 = sonar.ping(DigitalPin.P13, DigitalPin.P15, PingUnit.CENTIMETERS)
+                pause(500)
                 robotbit.geek_servo(robotbit.Servos.S1, 0)
-                if odleglosc2 < 10:
-                    robotbit.motor_run_dual(robotbit.Motors.M1A, -150, robotbit.Motors.M2A, -150)
-                    pause(1000)
-                    robotbit.motor_run_dual(robotbit.Motors.M1A, 0, robotbit.Motors.M2A, 0)
-                    
+                if odleglosc1 < 10 and odleglosc2 < 10:
+                    break
+                if odleglosc1 > odleglosc2:
+                    robotbit.motor_run_dual(robotbit.Motors.M1A, 150, robotbit.Motors.M2A, 150)
+                    pause(500)
                 else:
+                    robotbit.motor_run_dual(robotbit.Motors.M1A, -150, robotbit.Motors.M2A, -150)
+                    pause(500)
 
-                    robotbit.geek_servo(robotbit.Servos.S1, 25)
-                    odleglosc2 = sonar.ping(DigitalPin.P13, DigitalPin.P15, PingUnit.CENTIMETERS)
-                    robotbit.geek_servo(robotbit.Servos.S1, 0)
-                    if odleglosc2 < 10:
-                        robotbit.motor_run_dual(robotbit.Motors.M1A, -150, robotbit.Motors.M2A, -150)
-                        robotbit.motor_run_dual(robotbit.Motors.M1A, -150, robotbit.Motors.M2A, -150)
-                        pause(1000)
-                        robotbit.motor_run_dual(robotbit.Motors.M1A, 0, robotbit.Motors.M2A, 0)
                         
+IR_V15.init(Pins.P8)
                         
-                    else:
-                        
-                        break
+      
 def on_button_pressed_a():
     jedzdotylu()
     pass
 input.on_button_pressed(Button.A, on_button_pressed_a)
-        
-                
-
+def on_press_event_ch_minus():
+    jedzdotylu()
+    pass
+IR_V15.on_press_event(RemoteButton.NEXT, on_press_event_ch_minus)
--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 function jedzdotylu() {
     let odleglosc2: number;
+    let odleglosc1: number;
     while (true) {
         robotbit.MotorRunDual(robotbit.Motors.M1A, -150, robotbit.Motors.M2A, 150)
         pause(1000)
@@ -8,24 +9,21 @@ function jedzdotylu() {
         if (odleglosc2 < 10) {
             robotbit.GeekServo(robotbit.Servos.S1, -25)
             odleglosc2 = sonar.ping(DigitalPin.P13, DigitalPin.P15, PingUnit.Centimeters)
+            pause(500)
+            robotbit.GeekServo(robotbit.Servos.S1, 25)
+            odleglosc1 = sonar.ping(DigitalPin.P13, DigitalPin.P15, PingUnit.Centimeters)
+            pause(500)
             robotbit.GeekServo(robotbit.Servos.S1, 0)
-            if (odleglosc2 < 10) {
-                robotbit.MotorRunDual(robotbit.Motors.M1A, -150, robotbit.Motors.M2A, -150)
-                pause(1000)
-                robotbit.MotorRunDual(robotbit.Motors.M1A, 0, robotbit.Motors.M2A, 0)
+            if (odleglosc1 < 10 && odleglosc2 < 10) {
+                break
+            }
+            
+            if (odleglosc1 > odleglosc2) {
+                robotbit.MotorRunDual(robotbit.Motors.M1A, 150, robotbit.Motors.M2A, 150)
+                pause(500)
             } else {
-                robotbit.GeekServo(robotbit.Servos.S1, 25)
-                odleglosc2 = sonar.ping(DigitalPin.P13, DigitalPin.P15, PingUnit.Centimeters)
-                robotbit.GeekServo(robotbit.Servos.S1, 0)
-                if (odleglosc2 < 10) {
-                    robotbit.MotorRunDual(robotbit.Motors.M1A, -150, robotbit.Motors.M2A, -150)
-                    robotbit.MotorRunDual(robotbit.Motors.M1A, -150, robotbit.Motors.M2A, -150)
-                    pause(1000)
-                    robotbit.MotorRunDual(robotbit.Motors.M1A, 0, robotbit.Motors.M2A, 0)
-                } else {
-                    break
-                }
-                
+                robotbit.MotorRunDual(robotbit.Motors.M1A, -150, robotbit.Motors.M2A, -150)
+                pause(500)
             }
             
         }
@@ -33,7 +31,12 @@ function jedzdotylu() {
     }
 }
 
+IR_V15.init(Pins.P8)
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    jedzdotylu()
+    
+})
+IR_V15.onPressEvent(RemoteButton.NEXT, function on_press_event_ch_minus() {
     jedzdotylu()
     
 })
